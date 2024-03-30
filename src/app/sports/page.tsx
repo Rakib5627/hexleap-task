807eea5e401d7c093a5032818fcddd2f98986833
Rakibul Islam <mdrakib.5627@gmail.com>
2024-03-30 15:02:23 +0600

    code organized and added button

--- src/app/sports/page.tsx
+++ src/app/sports/page.tsx
@@ -4,93 +4,51 @@ import newjersey from "../../../public/new-jersey.jpg"
 import ad from "../../../public/ad.jpg"
 import Image from "next/image";
 const Sports = () => {
+
+    const sportsData = [
+        { imageSrc: rivercat, title: "Sacramento River Cats", totalEvents: 48, sport: "Baseball" },
+        { imageSrc: lasvegas, title: "Las Vegas Aviators", totalEvents: 28, sport: "Baseball" },
+        { imageSrc: newjersey, title: "New Jersey Devils", totalEvents: 15, sport: "Baseball" },
+        { imageSrc: lasvegas, title: "Las Vegas Aviators", totalEvents: 28, sport: "Baseball" }
+    ];
+
+    const advertisementData = {
+        imageSrc: ad,
+        title: "Advertisement Title",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    };
+
+
     return (
         <div>
             <h3 className="font-bold text-2xl py-5">Sports</h3>
             <div className="flex justify-center gap-3">
-                <div className=" bg-white p-4">
-                    <Image src={rivercat} alt="Example Image" width={218} height={385}/>
-                    <h5 className="my-4 font-medium text-sm">Sacramento River Cats</h5>
-                    <div style={{ backgroundColor: '#F7F7F8' }} className=" p-3 gap-2 text-xs flex justify-between">
-                        <div className=" w-1/2" >
-                            <p>Total Event</p>
-                            <p className=" font-medium mt-1">48 Events</p>
-                        </div>
-                        <div className="w-1/2">
-                            <p>Sport</p>
-                            <p className=" font-medium mt-1">Baseball</p>
-                        </div>
-                    </div>
-                </div>
-                <div className=" bg-white p-4">
-                    <Image
-                        src={lasvegas}
-                        alt="Example Image"
-                        width={218}
-                        height={385}
-                    />
-                    <h5 className="my-4 font-medium text-sm">Las Vegas Aviators</h5>
-                    <div style={{ backgroundColor: '#F7F7F8' }} className=" p-3 gap-2 text-xs flex justify-between">
-                        <div className=" w-1/2" >
-                            <p>Total Event</p>
-                            <p className=" font-medium mt-1">48 Events</p>
-                        </div>
-                        <div className="w-1/2">
-                            <p>Sport</p>
-                            <p className=" font-medium mt-1">Baseball</p>
+                {sportsData.map((data, index) => (
+                    <div className="bg-white p-3" key={index}>
+                        <Image src={data.imageSrc} alt={data.title} width={218} height={385} />
+                        <h5 className="my-4 font-medium text-sm">{data.title}</h5>
+                        <div style={{ backgroundColor: '#F7F7F8' }} className="p-3 gap-2 text-xs flex justify-between">
+                            <div className="w-1/2">
+                                <p>Total Event</p>
+                                <p className="font-medium mt-1">{data.totalEvents} Events</p>
+                            </div>
+                            <div className="w-1/2">
+                                <p>Sport</p>
+                                <p className="font-medium mt-1">{data.sport}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className=" bg-white p-4">
-                    <Image
-                        src={newjersey}
-                        alt="Example Image"
-                        width={218}
-                        height={385}
-                    />
-                    <h5 className="my-4 font-medium text-sm">New Jersey Devils</h5>
-                    <div style={{ backgroundColor: '#F7F7F8' }} className=" p-3 gap-2 text-xs flex justify-between">
-                        <div className=" w-1/2" >
-                            <p>Total Event</p>
-                            <p className=" font-medium mt-1">48 Events</p>
-                        </div>
-                        <div className="w-1/2">
-                            <p>Sport</p>
-                            <p className=" font-medium mt-1">Baseball</p>
-                        </div>
-                    </div>
-                </div>
-                <div className=" bg-white p-4">
-                    <Image
-                        src={lasvegas}
-                        alt="Example Image"
-                        width={218}
-                        height={385}
-                    />
-                    <h5 className="my-4 font-medium text-sm">Las Vegas Aviators</h5>
-                    <div style={{ backgroundColor: '#F7F7F8' }} className=" p-3 gap-2 text-xs flex justify-between">
-                        <div className=" w-1/2" >
-                            <p>Total Event</p>
-                            <p className=" font-medium mt-1">48 Events</p>
-                        </div>
-                        <div className="w-1/2">
-                            <p>Sport</p>
-                            <p className=" font-medium mt-1">Baseball</p>
-                        </div>
-                    </div>
-                </div>
-                <div className=" bg-white p-4">
-                    <Image
-                        src={ad}
-                        alt="Example Image"
-                        width={218}
-                        height={385}
-                    />
-                    <h5 className="my-4 font-medium text-sm">Advertisement Title</h5>
+                ))}
+                <div className="bg-white p-4">
+                    <Image src={advertisementData.imageSrc} alt={advertisementData.title} width={218} height={385} />
+                    <h5 className="my-4 font-medium text-sm">{advertisementData.title}</h5>
                     <div style={{ width: '218px' }}>
-                      <p className="text-xs">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p className="text-xs">{advertisementData.content}</p>
                     </div>
                 </div>
+            </div>
+            <div className="flex justify-center mt-6">
+                <button style={{ backgroundColor: '#2C9CF0' }} className="text-white text-xs px-5 py-2 rounded" >See More</button>
             </div>
         </div>
     );
